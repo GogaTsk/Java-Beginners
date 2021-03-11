d400ef7090a5e0d8d2c56b1c31b32f6bfc23af4c
GogaTsk <gogi.wkrialashvili@gmail.com>
2021-03-11 09:31:03 -0800

    add some changes

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log ("დავალება1")
+var x=14
+var y=33
+var result  = x + y
+console.log (result)
+var c=x+y
+
+if(c%2===0) {
+    console.log('c is even')
+}else if(c%2===1){
+    console.log('c is odd')
+}else if(c%2===-1){
+    console.log('c is odd')
+}else{
+    console.log('c is unknown type')
+}
+
+var b=10
+var h=7
+var result = b*h
+console.log (result)
+var a=1/2*b*h
+
+if(a>0){
+    console.log(a)
+}else if(a<0){
+    console.log(-a)
+}else{
+    console.log('a is unknown type')
+}
